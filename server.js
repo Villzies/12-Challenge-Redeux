555124185ee426e89eca4ffa878cb85016b93c0b
Villzies <gemberling.stephan@att.net>
2023-11-26 19:59:12 -0600

    Added question for initial prompt

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const inquirer = require('inquirer')
+const inquirer = require('inquirer');
 const mysql = require('mysql2');
-
+const questions = require('questions');
 const db = mysql.createConnection(
     {
       host: "localhost",
@@ -11,5 +11,49 @@ const db = mysql.createConnection(
     },
   );
 
-  
+  function init() {
+    inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "choice",
+        message: "Please select what you would like to do",
+        choices: [
+          "View All Employees",
+          "Add an Employee",
+          "Update an Employee Role",
+          "View All Roles",
+          "Add Role",
+          "View All Departments",
+          "Add a Department",
+        ],
+        loop: true,
+      },
+    ])
+    .then((answers) => {
+        const selectedAction = answers.action;
+        if (selectedAction === "View All Employees") {
+          viewAllEmployees();
+        } else if (selectedAction === "Add an Employee") {
+          addEmployee();
+        } else if (selectedAction === "Update an Employee Role") {
+          updateEmployeeRole();
+        } else if (selectedAction === "View All Roles") {
+          viewAllRoles();
+        } else if (selectedAction === "Add a Role") {
+          addRole();
+        } else if (selectedAction === "View All Departments") {
+          viewAllDepartments();
+        } else if (selectedAction === "Add a Department") {
+          addDepartment();
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
 
+
+
+  
+init();
