0bc595a8dd2e0010c2f82ffed1b57864376b4379
Villzies <gemberling.stephan@att.net>
2023-11-26 18:54:18 -0600

    Added department and title seeds

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO department (name) 
+VALUES ('Guest Service'),
+('Domestics'),
+('Fulfillment'),
+('Grocery'),
+('Beauty');
+
+INSERT INTO role (title, salary, department_id) 
+VALUES
+('Service Advocate', 30000.00, 1),
+('Freight Unloader', 45000.00, 2),
+('Fulfillment Advocate', 30000.00, 3),
+('Grocery Inventory Auditor', 100000.00, 4)
+('Makeup Artist', 50000.00, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
