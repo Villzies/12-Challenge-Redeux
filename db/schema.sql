3a8b45c1d230f04ad73bd67c65dc88c94ee9afbc
Villzies <gemberling.stephan@att.net>
2023-11-26 19:31:34 -0600

    Messing around with sql

--- db/schema.sql
+++ db/schema.sql
@@ -3,13 +3,13 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-    id INT NOT NULL,
-    name VARCHAR(30),
-    PRIMARY KEY (id)
+id INT NOT NULL AUTO_INCREMENT,
+title VARCHAR(30),
+PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-id INT NOT NULL,
+id INT NOT NULL AUTO_INCREMENT,
 title VARCHAR(30),
 salary DECIMAL,
 department_id INT,
@@ -17,10 +17,11 @@ PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
+id INT NOT NULL AUTO_INCREMENT,
 first_name VARCHAR(30),
 last_name VARCHAR(30),
 role_id INT NOT NULL,
 manager_id INT,
-PRIMARY KEY (id) REFERENCES role(id),
+PRIMARY KEY (id),
 FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,12 @@
-INSERT INTO department (name) 
-VALUES ('Guest Service'),
+INSERT INTO department
+VALUES 
+('Guest Service'),
 ('Domestics'),
 ('Fulfillment'),
 ('Grocery'),
 ('Beauty');
 
-INSERT INTO role (title, salary, department_id) 
+INSERT INTO role (id, title, salary, department_id) 
 VALUES
 ('Service Advocate', 30000.00, 1),
 ('Freight Unloader', 45000.00, 2),
@@ -13,7 +14,7 @@ VALUES
 ('Grocery Inventory Auditor', 100000.00, 4)
 ('Makeup Artist', 50000.00, 5);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES
 ('Chris', 'Eric', 1, 1),
 ('Bridgette', 'Stow', 2, 1),
